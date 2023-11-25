e64afbc29e9663b0b2ab785c32d3532d0f5f1988
Ivan Dobrokhvalov <dobrokhvalov.official@ya.ru>
2023-11-25 13:01:49 +0300

    Add Code data classes and conversion methods

---
+++ app/src/main/java/com/example/mts_music/data/Code.kt
@@ -0,0 +1,3 @@
+package com.example.mts_music.data
+
+data class Code(val code: String)
--- app/src/main/java/com/example/mts_music/data/Converter.kt
+++ app/src/main/java/com/example/mts_music/data/Converter.kt
@@ -2,15 +2,17 @@ package com.example.mts_music.data
 
 class Converter {
 
+    // User
     private fun convertUserToUserSerialization(user: User): UserSerialization {
-        return UserSerialization(user.phoneNumber, user.username, user.sms)
+        return UserSerialization(user.phoneNumber, user.username)
     }
 
     private fun convertUserSerializationToUser(user: UserSerialization): User {
-        return User(user.phoneNumber, user.username, user.sms)
+        return User(user.phoneNumber, user.username)
     }
 
-    fun convertRoomToRoomSerialization(room: Room): RoomSerialization{
+    // Room
+    fun convertRoomToRoomSerialization(room: Room): RoomSerialization {
         val usersSerialization = room.users.map {
             convertUserToUserSerialization(it)
         }
@@ -22,5 +24,14 @@ class Converter {
             convertUserSerializationToUser(it)
         }
         return Room(room.author, users, room.access)
+    }
+
+    // Code
+    fun convertCodeToCodeSerialization(code: Code): CodeSerialization {
+        return CodeSerialization(code.code)
+    }
+
+    fun convertCodeSerializationToCode(code: CodeSerialization): Code {
+        return Code(code.code)
     }
 }
