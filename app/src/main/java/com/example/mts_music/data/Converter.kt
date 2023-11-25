deb07a30b5d3c6d0b35fe3600b294a59d8310905
Ivan Dobrokhvalov <dobrokhvalov.official@ya.ru>
2023-11-25 11:50:00 +0300

    Replacing Gson with Json

--- app/src/main/java/com/example/mts_music/data/Converter.kt
+++ app/src/main/java/com/example/mts_music/data/Converter.kt
@@ -1,21 +1,22 @@
 package com.example.mts_music.data
 
-import com.google.gson.Gson
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 class Converter {
-    private val gson = Gson()
+    private val json = Json { encodeDefaults = true }
 
     fun convertUserToUserSerialization(user: User): UserSerialization {
         return UserSerialization(user.phoneNumber, user.username, user.sms)
     }
 
     fun convertUserSerializationToJson(user: UserSerialization): String {
-        return gson.toJson(user)
+        return json.encodeToString(user)
     }
 
-    fun convertJsonToUserSerialization(jsonString: String): UserSerialization {
-        return gson.fromJson(jsonString, UserSerialization::class.java)
-    }
+//    fun convertJsonToUserSerialization(jsonString: String): UserSerialization {
+//        return json.decodeFromString<UserSerialization>(jsonString)
+//    }
 
     fun convertUserSerializationToUser(user: UserSerialization): User {
         return User(user.phoneNumber, user.username, user.sms)
