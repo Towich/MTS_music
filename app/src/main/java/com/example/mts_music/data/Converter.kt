db3a1f57b02c964ae30fd8a9e2c13f0314d4d21f
Ivan Dobrokhvalov <dobrokhvalov.official@ya.ru>
2023-11-25 11:54:19 +0300

    Remove Json methods in Converter

--- app/src/main/java/com/example/mts_music/data/Converter.kt
+++ app/src/main/java/com/example/mts_music/data/Converter.kt
@@ -1,18 +1,15 @@
 package com.example.mts_music.data
 
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-
 class Converter {
-    private val json = Json { encodeDefaults = true }
+//    private val json = Json { encodeDefaults = true }
 
     fun convertUserToUserSerialization(user: User): UserSerialization {
         return UserSerialization(user.phoneNumber, user.username, user.sms)
     }
 
-    fun convertUserSerializationToJson(user: UserSerialization): String {
-        return json.encodeToString(user)
-    }
+//    fun convertUserSerializationToJson(user: UserSerialization): String {
+//        return json.encodeToString(user)
+//    }
 
 //    fun convertJsonToUserSerialization(jsonString: String): UserSerialization {
 //        return json.decodeFromString<UserSerialization>(jsonString)
